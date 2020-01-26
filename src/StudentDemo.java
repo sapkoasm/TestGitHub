5d4ad9137c667f5e98357c751290f1afa73f1d51
sapkoasm <sapkoasm@sheridancollege.ca>
2020-01-26 16:06:02 -0500

    initial commit for new Student code, the change is in StudentDemo

---
+++ src/StudentDemo.java
@@ -0,0 +1,55 @@
+/**
+ * This class +++Insert Description Here+++
+ * 
+ * @author Asmita Sapkota
+ */
+public class StudentDemo {
+    
+  private String name;
+  private int age;
+  private boolean graduate;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the graduate
+     */
+    public boolean isGraduate() {
+        return graduate;
+    }
+
+    /**
+     * @param graduate the graduate to set
+     */
+    public void setGraduate(boolean graduate) {
+        this.graduate = graduate;
+    }
+
+
+}
